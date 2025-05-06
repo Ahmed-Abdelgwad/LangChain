e3eae9660ad0f90cd5014165d88c684eef8b091f
AhmedAbdulJawad <146244420+Ahmed-Abdelgwad@users.noreply.github.com>
2025-05-06 18:23:40 +0300

    Delete Langchain directory

--- Langchain/oll.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.llms import Ollama  # Adjust to match the correct class name
-
-llm = Ollama(model="llama3.1")
-response = llm.invoke("Who won the 2010 World Cup?")
-print(response)
